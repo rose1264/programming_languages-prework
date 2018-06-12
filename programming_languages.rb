05930aa409713fc21ea661bc2cb63d3ff4a546e0
Shumei Huang <rose1264@gmail.com>
2018-06-12 12:49:11 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -36,7 +36,9 @@ def reformat_languages(languages)
   languages.each do |oo_or_functional_key, programming_languages_hash_value|
     programming_languages_hash_value.each do |language_name_key, type_hash_value|
       type_hash_value.each do |type_key, type_value|
-
+        if new_hash[language_name_key].nil?
+          language_name_key = {}
+        end
         new_hash[language_name_key][:style] ||= []
         new_hash[language_name_key][:style] << oo_or_functional_key
       end
